7030e1b5777f0fa079dec8f498610adb7536bfb6
Kenny Vo <28298773+vokenny@users.noreply.github.com>
2022-06-23 17:04:48 +0100

    feat: adjust styling the search component

--- src/components/search/search.component.ts
+++ src/components/search/search.component.ts
@@ -1,13 +1,23 @@
 import './search.style.css';
 
 class Search {
-  $search: HTMLInputElement = document.createElement('input');
+  private $search: HTMLFormElement = document.createElement('form');
+  private $lookup: HTMLInputElement = document.createElement('input');
+  private $lookupBtn: HTMLButtonElement = document.createElement('button');
 
   constructor() {
-    this.$search.id = 'search';
+    this.$lookup.id = 'lookup';
+    this.$lookup.placeholder = 'Forecast lookup';
+
+    this.$lookupBtn.id = 'lookup-btn';
+    this.$lookupBtn.className = 'glass';
+    this.$lookupBtn.textContent = 'Search';
+
+    this.$search.id = 'forecast-search';
+    this.$search.replaceChildren(this.$lookup, this.$lookupBtn);
   }
 
-  update(): HTMLInputElement {
+  update(): HTMLFormElement {
     return this.$search;
   }
 }

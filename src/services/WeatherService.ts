f57f15449f9eaa8fa5b1779b26c62be95db6ea8e
Kenny Vo <28298773+vokenny@users.noreply.github.com>
2022-06-25 11:39:56 +0100

    refactor: move base url to config.json

--- src/services/WeatherService.ts
+++ src/services/WeatherService.ts
@@ -1,8 +1,9 @@
-import { QueryParams } from '../types';
+import { Config, QueryParams } from '../types';
 
 class WeatherService {
-  private baseUrl: string = 'https://api.openweathermap.org/data/2.5/weather';
-  private apiKey: string = require('../../config.json')['API_KEY'];
+  private config: Config = require('../../config.json');
+  private baseUrl: string = this.config.BASE_URL;
+  private apiKey: string = this.config.API_KEY;
 
   async getNewForecast(queryParams: QueryParams): Promise<Response> {
     const url: string = `${this.baseUrl}?apikey=${this.apiKey}`;
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,5 +1,10 @@
 import { UnitSystem } from './constants/Unit';
 
+type Config = {
+  BASE_URL: string;
+  API_KEY: string;
+};
+
 type QueryParams = {
   q?: string;
   units?: UnitSystem;
